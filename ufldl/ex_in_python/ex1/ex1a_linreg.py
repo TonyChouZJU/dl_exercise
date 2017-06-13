2f1efeba53e4d519a8f4063233bbc6c5a646e584
TonyChouZJU <whzyb1991@163.com>
2017-06-13 20:49:06 +0800

    add three different optimize

--- ufldl/ex_in_python/ex1/ex1a_linreg.py
+++ ufldl/ex_in_python/ex1/ex1a_linreg.py
@@ -3,6 +3,40 @@ import scipy.optimize
 import time
 import matplotlib.pylab as plt
 
+from sklearn import preprocessing
+
+import sys
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+class featureNormlize():
+    def __init__(self, X, method='stand'):
+        self._method = method
+        self._scaler = self.initScaler(X)
+
+    def initScaler(self, X):
+        if self._method=='stand':
+            return preprocessing.StandardScaler().fit(X)
+        #default min and max is 0 and 1
+        #If MinMaxScaler is given an explicit feature_range=(min, max) the full formula is:
+        #X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+        #X_scaled = X_std / (max - min) + min
+        elif self._method=='minmax':
+            return preprocessing.MinMaxScaler()
+        elif self._method=='maxabs':
+            return preprocessing.MaxAbsScaler()
+        elif self._method=='l2':
+            return preprocessing.Normalizer.fit(X, norm='l2')
+        elif self._method=='l1':
+            return preprocessing.Normalizer.fit(X, norm='l1')
+        else:
+            return None
+    def transform(self, x):
+        if self._scaler != 'None':
+            return self._scaler.transform(x)
+        else:
+            return None
+
 def linear_regression(theta,X,y):
 	y_preds = X.dot(theta)
 	return y_preds
@@ -10,28 +44,43 @@ def linear_regression(theta,X,y):
 def cost_function(theta, X, y):
 	y_preds = linear_regression(theta, X, y)
 	loss = np.sum(np.square(y_preds-y))/(2*y.shape[0])
+	#loss = np.sum(np.square(y_preds-y))/2
 	return loss
 
 def gradient(theta, X, y):
 	y_preds = linear_regression(theta, X, y)
 	errors = y_preds-y
 
-	theta_grad = errors.dot(X)/ y.shape[0]
+	theta_grad = (errors.T).dot(X)/ y.shape[0]
 	return theta_grad
 
 
 def optimizer(theta_init, X, y, iters=200):
+        loss_history = []
 	theta = theta_init
-	c = 0.01
+	c = 0.1
 	#for i in range(X.shape[1]):
 	for i in range(iters):
-		X_i = X[i%X.shape[0], :].reshape((-1,theta.shape[0]))
-		y_i = y[i%X.shape[0], :].reshape((-1,1))
+                if i == int(iters*0.5):
+                    c = c*0.1
+                elif i == int(iters*0.7):
+                    c = c*0.1
+                elif i == int(iters*0.9):
+                    c = c*0.1
+
+
+		#X_i = X[i%X.shape[0], :].reshape((-1,theta.shape[0]))
+		X_i = X
+		#y_i = y[i%X.shape[0]].reshape((-1,1))
+                #y_i = y[i%X.shape[0]].reshape(1)
+                y_i = y
 		loss = cost_function(theta, X_i, y_i)
 		theta_grad = gradient(theta, X_i, y_i)
 		theta = theta -  c * theta_grad.transpose()
-		print loss,'\t\t\t', theta.flatten()
-	return theta
+                loss_history.append(loss)
+                logging.info("iter: {:8d}\n\t\t\tloss: {:6f}".format(i, loss))
+                logging.info( '\t\t\tRMS error: {error}'.format( error=np.sqrt(np.mean( (linear_regression(theta, X_i, y_i) - y_i) ** 2))) )
+	return theta, loss_history
 
 def load_data(input_file='housing.data'):
 	np_data= np.zeros((0,14),np.float32)
@@ -41,57 +90,119 @@ def load_data(input_file='housing.data'):
 			this_line = [float(p) for p in line.strip().split()]
 			np_data = np.vstack([np_data, np.array(this_line)])
 	
-	np_data = np.hstack([np.ones((np_data.shape[0], 1)), np_data])
+	#np_data = np.hstack([np.ones((np_data.shape[0], 1)), np_data])
 	return np_data
 
-dataset = load_data(input_file='housing.data')
-#dataset = load_data(input_file='test.data')
-print 'dataset:', dataset
-data_size = dataset.shape[0]
-train_ratio = 1
-train_size= int(data_size * train_ratio)
+def split_data(dataset, train_ratio=0.8):
+    data_size = dataset.shape[0]
+    train_size= int(data_size * train_ratio)
+    
+    train_x = dataset[:train_size, :-1]
+    train_y = dataset[:train_size, -1]
+    
+    test_x = dataset[train_size:, :-1]
+    test_y = dataset[train_size:, -1]
+    return train_x, train_y, test_x, test_y
 
-train_x = dataset[:train_size, :-1]
-train_y = dataset[:train_size, -1:]
 
-test_x = dataset[train_size:, :-1]
-test_y = dataset[train_size:, -1:]
+def show_J_history(J_history):
+    plt.plot(J_history, marker='o')
+    plt.title("Theta History")
+    plt.xlabel('Iterations')
+    plt.ylabel('J(theta)')
+    plt.show()
 
-theta_init = np.random.rand(train_x.shape[1], 1)
+def show_results(test_y, y_preds, sv_name):
+    plt.figure(figsize=(10, 8))
+    plt.scatter(np.arange(test_y.size), sorted(test_y), c='r', edgecolor='None', alpha=0.5, label='Actual')
+    plt.scatter(np.arange(test_y.size), sorted(y_preds), c='b', edgecolor='None', alpha=0.5, label='Predicted')
+    range_y = int(max(test_y)-min(test_y))
+    plt.legend(loc='upper left')
+    plt.ylim([min(test_y)-0.1*range_y, max(test_y)+0.1*range_y])
+    plt.title("Predicted vs. Actual House Price")
+    plt.ylabel('Price ($1000s)')
+    plt.xlabel('House #')
+#    plt.show()
+    plt.savefig(sv_name)
 
-#print 'loss:\t\t theta:'
-#theta_final = optimizer(theta_init, train_x, train_y, iters=200)
+def gradient_descend(train_x, train_y, test_x, test_y, theta_init, sv_name,  iters=200):
+    theta_final, J_history = optimizer(theta_init, train_x, train_y, iters)
+    			
+    pred_prices = linear_regression(theta_final, test_x, test_y)
 
-#print 'theta init:',  theta_init.flatten()
-#print 'theta final:', theta_final.flatten()
-			
-J_history = []
+    show_J_history(J_history)
+    show_results(test_y, pred_prices, sv_name)
+    return theta_final
+    
+def scipy_optimize(train_x, train_y, test_x, test_y,  theta_init, sv_name, iters=200):
+    J_history = []
+    
+    t0 = time.time()
+    res = scipy.optimize.minimize(
+        fun=cost_function,
+        x0=theta_init,
+        args=(train_x, train_y),
+        method='bfgs',
+        jac=gradient,
+        options={'maxiter': iters, 'disp': True},
+        callback=lambda x: J_history.append(cost_function(x, train_x, train_y)),
+    )
+    t1 = time.time()
+    
+    optimal_theta = res.x
 
-t0 = time.time()
-#res = scipy.optimize.minimize(
-#    fun=cost_function,
-#    x0=np.random.rand(train_x.shape[1], 1 ),
-#    args=(train_x, train_y),
-#    method='bfgs',
-#    jac=gradient,
-#    options={'maxiter': 200, 'disp': True},
-#    callback=lambda x: J_history.append(cost_function(x, train_x, train_y)),
-#)
-res = scipy.optimize.minimize(
-    fun=cost_function,
-    x0=np.random.rand(train_x.shape[1] ),
-    args=(train_x, train_y),
-    method='bfgs',
-    jac=gradient,
-    options={'maxiter': 200, 'disp': True},
-)
-t1 = time.time()
+    pred_prices = linear_regression(optimal_theta, test_x, test_y)
+    show_J_history(J_history)
+    show_results(test_y, pred_prices, sv_name)
+    return optimal_theta
 
-print('Optimization took {s} seconds'.format(s=t1 - t0))
-optimal_theta = res.x
-print optimal_theta
-print 'final loss:', cost_function(optimal_theta, train_x, train_y)
-plt.plot(J_history, marker='o')
-plt.xlabel('Iterations')
-plt.ylabel('J(theta)')
-plt.show()
+#y = theta*X
+#theta = (XTX)-1*XT*y
+def normal_equation(train_x, train_y, test_x, test_y, sv_name):
+    xt_x = (train_x.T).dot(train_x)
+    xt_x_inv = np.linalg.inv(xt_x)
+    theta = xt_x_inv.dot(train_x.T).dot(train_y)
+
+    pred_y = linear_regression(theta, test_x, test_y)
+    show_results(test_y, pred_y, sv_name)
+    return theta
+
+if __name__ == '__main__':
+    dataset = load_data(input_file='housing.data')
+    #print 'dataset:', dataset
+    np.random.shuffle(dataset)
+    
+    
+    train_x, train_y, test_x, test_y = split_data(dataset)
+    
+    normlizer = featureNormlize(train_x,'stand')
+
+    train_x = normlizer.transform(train_x)
+    train_x = np.hstack([np.ones((train_x.shape[0], 1)), train_x])
+    
+    test_x = normlizer.transform(test_x)
+    test_x = np.hstack([np.ones((test_x.shape[0], 1)), test_x])
+    
+    theta_init = np.random.rand(train_x.shape[1])
+
+    
+    #hand write gradient descend
+    optimal_theta_1 = gradient_descend(train_x, train_y, test_x, test_y, theta_init, sv_name='./results/gt_vs_pred_1.png', iters=200)
+    optimal_theta_2 = scipy_optimize(train_x, train_y, test_x, test_y, theta_init, sv_name='./results/gt_vs_pred_2.png',iters=200)
+
+    optimal_theta_3 = normal_equation(train_x, train_y, test_x, test_y, sv_name='./results/gt_vs_pred_3.png')
+    logging.info('Loss 1:{}'.format(cost_function(optimal_theta_1, train_x, train_y)))
+    logging.info('Loss 2:{}'.format(cost_function(optimal_theta_2, train_x, train_y)))
+    logging.info('Loss 3:{}'.format(cost_function(optimal_theta_3, train_x, train_y)))
+    
+    ###################
+    ## Error and Plots ##
+    ###################
+    
+    ## look at the root mean squared error
+    for df, (X, y) in (('train', (train_x, train_y)),('test', (test_x, test_y))):
+        actual_prices = y
+        predicted_prices = X.dot(optimal_theta_2)
+        logging.info('RMS {dataset} error: {error}'.format(dataset=df,
+                                                    error=np.sqrt(np.mean((predicted_prices - actual_prices) ** 2))         )
+        )
--- ufldl/ex_in_python/ex1/ex1a_linreg_scipy.py
+++ ufldl/ex_in_python/ex1/ex1a_linreg_scipy.py
@@ -14,22 +14,28 @@ train_y = data[:400, -1]
 
 test_X = data[400:, :-1]
 test_y = data[400:, -1]
+print 'X shape:', train_X.shape, ' y shape:',train_y.shape
+
 
 m, n = train_X.shape
 
 def cost_function(theta, X, y): 
     squared_errors = (X.dot(theta) - y) ** 2
     J = 0.5 * squared_errors.sum()
+    print 'IN cost:',' X.shape:',X.shape,' y.shape:',y.shape, ' theta:',theta.shape, ' errors.shape:', squared_errors.shape, ' cost.shape:', J.shape
     return J
 
 def gradient(theta, X, y):
     errors = X.dot(theta) - y
-    return errors.dot(X)
+    theta_delta = errors.dot(X)
+    print 'In gradient:',  'X.shape:',X.shape,' y.shape:',y.shape, ' theta:',theta.shape, ' errors.shape:', errors.shape, ' theta_delta.shape:', theta_delta.shape
+    return theta_delta
 
 
 print 'cost:', cost_function(np.random.rand(n), train_X, train_y)
 
-assert False
+def print_cost(x):
+    print 'theta:',x
 
 J_history = []
 
@@ -42,7 +48,9 @@ res = scipy.optimize.minimize(
     jac=gradient,
     options={'maxiter': 200, 'disp': True},
     callback=lambda x: J_history.append(cost_function(x, train_X, train_y)),
+    #callback=lambda x: print_cost(x),
 )
+print 'J_history:', J_history
 t1 = time.time()
 
 print('Optimization took {s} seconds'.format(s=t1 - t0))
@@ -51,4 +59,29 @@ optimal_theta = res.x
 plt.plot(J_history, marker='o')
 plt.xlabel('Iterations')
 plt.ylabel('J(theta)')
+plt.show()
+
+## look at the root mean squared error
+for df, (X, y) in (('train', (train_X, train_y)),('test', (test_X, test_y))):
+    actual_prices = y
+    predicted_prices = X.dot(optimal_theta)
+    print('RMS {dataset} error: {error}'.format(dataset=df,
+                                                error=np.sqrt(np.mean((predicted_prices - actual_prices) ** 2))         )
+    )
+
+
+## plotting the test data
+pred_prices = np.dot(test_X, optimal_theta)
+#pred_prices = linear_regression(optimal_theta, test_X, test_y)
+print 'pred_prices shape:',pred_prices.shape
+
+print 'test_y.size:', test_y.size
+print 'pred_y.size:', pred_prices.size
+plt.figure(figsize=(10, 8))
+plt.scatter(np.arange(test_y.size), sorted(test_y), c='r', edgecolor='None', alpha=0.5, label='Actual')
+plt.scatter(np.arange(test_y.size), sorted(pred_prices), c='b', edgecolor='None', alpha=0.5, label='Predicted')
+plt.legend(loc='upper left')
+plt.title("Predicted vs. Actual House Price")
+plt.ylabel('Price ($1000s)')
+plt.xlabel('House #')
 plt.show()
